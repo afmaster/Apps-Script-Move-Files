c03455a626cbdd62e52d8485b6d423c2b8cd57d4
afmaster <andre@franciscatto.com>
2021-08-24 15:04:00 -0300

    Update move-files.js

--- move-files.js
+++ move-files.js
@@ -1,68 +1,18 @@
 function MoviFiles() {
   
-  const parentFolder = DriveApp.getFolderById("1B3LhVe7Mx1E04yFk-YYYG_KXoyCDIeFl");
-  const destinationVivo = DriveApp.getFolderById("1IQ9n6COz_yq7srbRqSOVGtc26pdj1O7V");
-  const destinationShell = DriveApp.getFolderById("1ooH9GmMTfRhqOz0fKaaJhNvMqk0-VJsF");
-  const destinationUber = DriveApp.getFolderById("1ZLeSJMQBDzLDup8yG2ib-wDR-kiarBOx");
-  const destinationCeee = DriveApp.getFolderById("1kQL2D3GP_SOfxuyH_bngHE9RGyXdleTP");
-  const destinationGas = DriveApp.getFolderById("1GxXq8Ht8HyodJPsCVnJQauGGY05aa2Hk");
-  const destinationEscola = DriveApp.getFolderById("1xonR-XIkTrfDrAY863v_fXeZXmCG51m5");
-  const destinationOperados = DriveApp.getFolderById("1kEjSGZOyemkuMkztfUja9Vc_tNoBq9kY");
+  const parentFolder = DriveApp.getFolderById("ID OF 'FROM' FOLDER");
+  const destinationFolder = DriveApp.getFolderById("ID OF DESTINATION FOLDER");
+
 
   let files = parentFolder.getFiles();
     while (files.hasNext()) {
       let file = files.next();
       let fileStr = file.toString();
       Logger.log(fileStr);
+      
+      // IF one needs to add some criteria based on file's name (otherwise just remove de 'if' statement):
       if (fileStr.split(".").includes("bl")){
-        destinationVivo.addFile(file);
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split(" ").includes("cx.txt")) {
-        destinationOperados.addFile(file);
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split(".").includes("txt")) {
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split(" ").includes("Supergasbras")) {
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split("_").includes("Boleto")) {
-        destinationVivo.addFile(file);
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split(" ").includes("compra")) {
-        destinationShell.addFile(file);
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split(" ").includes("Uber")) {
-        destinationUber.addFile(file);
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split(" ").includes("e-mail")) {
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split("-").includes("01")) {
-        destinationCeee.addFile(file);
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split("_").includes("000002639144")) {
-        destinationGas.addFile(file);
-        Logger.log(file);
-        parentFolder.removeFile(file);
-      }
-      else if (fileStr.split(" ").includes("Escola")) {
-        destinationEscola.addFile(file);
+        destinationFolder.addFile(file);
         Logger.log(file);
         parentFolder.removeFile(file);
       }
